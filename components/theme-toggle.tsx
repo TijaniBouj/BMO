109b4be1dda4c594fe8ac674b1ece5052280d4b9
Tijani <boujdaria.tijani@esprit.tn>
2024-07-03 02:54:09 +0100

    last

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -10,22 +10,5 @@ export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
   const [_, startTransition] = React.useTransition()
 
-  return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => {
-        startTransition(() => {
-          setTheme(theme === 'light' ? 'dark' : 'light')
-        })
-      }}
-    >
-      {!theme ? null : theme === 'dark' ? (
-        <IconMoon className="transition-all" />
-      ) : (
-        <IconSun className="transition-all" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
-  )
+  
 }
